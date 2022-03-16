54b682779992634cffa6d6cb0a0fbca60a8d5f26
Gaurav Saini <sainigaurav31200@gmail.com>
2022-03-16 22:42:37 +0530

    Added transfer money page

--- client/src/Pages/TransferMoney.js
+++ client/src/Pages/TransferMoney.js
@@ -1,5 +1,25 @@
 import React from "react";
 
 export default function TransferMoney() {
-  return <div>TransferMoney</div>;
+  const submitHandler = (event) => {
+    event.preventDefault();
+    console.log("Submitting");
+  };
+  return (
+    <div className="Transfer">
+      <form onSubmit={submitHandler}>
+        <h1>TRANSFER MONEY</h1>
+        <h2>Info</h2>
+        <input id="sender" type="text" required placeholder="Sender's Name" />
+        <input
+          id="rexipient"
+          type="text"
+          required
+          placeholder="Recipient's Name"
+        />
+        <input id="amount" type="number" required placeholder="Amount" />
+        <button type="submit">Transfer</button>
+      </form>
+    </div>
+  );
 }
